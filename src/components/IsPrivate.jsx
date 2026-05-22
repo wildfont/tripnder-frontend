9d356e3aecde9aae936290bcfad53d3aaad177e8
wildfont <jordifont4@gmail.com>
2026-05-22 17:29:18 +0200

    landing page started

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -2,14 +2,10 @@ import { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
-
 function IsPrivate(props) {
-  
-    const { isLoggedIn } = useContext(AuthContext);
-  
-    return isLoggedIn ? props.children : <Navigate to="/login" />
+  const { isLoggedIn } = useContext(AuthContext);
 
-
+  return isLoggedIn ? props.children : <Navigate to="/login" />;
 }
 
-export default IsPrivate
+export default IsPrivate;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,22 @@
-function landingPage() {
+import { Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+function LandingPage() {
+  const navigate = useNavigate()
   return (
-    <div>landingPage</div>
-  )
+    <div>
+      <img src={logo} style={{width:"100px"}} alt="Tripnder logo" />
+      <Typography>
+        <h1>Find your perfect travel companion</h1>
+        <h3>
+          "Match with solo travelers going your way. No dating vibes <br />— just
+          great adventures."
+        </h3>
+      </Typography>
+      <Button onClick={() => navigate("/signup")}>Start exploring</Button>
+      <Button onClick={() => navigate("/login")}>Login</Button>
+    </div>
+  );
 }
-export default landingPage
+export default LandingPage;
